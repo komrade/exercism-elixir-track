959612f96bbffd6df99a99c89fe4004a9593d29e
Pavel Timofeev <pav.timofeev@gmail.com>
2019-01-23 11:54:11 -0300

    Rotational Cipher

---
+++ rotational-cipher/rotational_cipher.exs
@@ -0,0 +1,22 @@
+defmodule RotationalCipher do
+  @doc """
+  Given a plaintext and amount to shift by, return a rotated string.
+
+  Example:
+  iex> RotationalCipher.rotate("Attack at dawn", 13)
+  "Nggnpx ng qnja"
+  """
+  @spec rotate(text :: String.t(), shift :: integer) :: String.t()
+  def rotate(text, shift) do
+    text
+    |> to_charlist
+    |> Enum.map(fn(char) -> 
+      cond do
+        char in ?a..?z -> ?a + rem(char - ?a + shift, 26)
+        char in ?A..?Z -> ?A + rem(char - ?A + shift, 26)
+        true -> char
+      end
+    end)
+    |> to_string
+  end
+end
